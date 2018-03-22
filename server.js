749bd928b0ab5c12a75e4d642f08af35fafca0cb
muhammad shahrukh <developer_shahrukh@hotmail.com>
2018-03-22 16:57:25 +0500

    * npm module added for parsing csv file * database created with mock data

--- server.js
+++ server.js
@@ -1,29 +1,74 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const PORT = 4000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const papaParse = require('papaparse');
 
 const express = require('express');
 const app = express();
 
+const Product = require('./api/product/product.model');
+
+// connecting to database
 mongoose.connect('mongodb://localhost/Product-Demo');
-mongoose.connection.on('error', function(err) {
+mongoose.connection.on('error', function (err) {
     console.error('MongoDB connection error: ' + err);
     process.exit(-1);
 });
 
+// initializing body parser
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 require('./routes')(app);
 
 app.route('/*')
-.get(function(req, res) {
-    res.sendFile(path.resolve( __dirname + '/client/index.html'));
-});
+    .get(function (req, res) {
+        res.sendFile(path.resolve(__dirname + '/client/index.html'));
+    });
 
-app.listen(PORT,()=>{
-    console.log('Server listening on PORT : '+PORT);
+// this method will create product mock data into our database
+async function createDatabase() {
+
+    try{
+    
+        // fetching data from csv file
+        var file = fs.readFileSync(path.join(__dirname, '/assets/mock-data.csv'), { encoding: 'utf8' });
+        // parsing data into key,value pair into an array
+        var parsedResult = papaParse.parse(file,{
+            header: true,
+            dynamicTyping: true,
+            encoding: "UTF-8",
+            delimiter: ",",
+            quoteChar: '"',
+            newline: '\n',
+            skipEmptyLines: true
+        });
+
+        parsedResult.data.forEach(function(d){
+            d['_id'] = d['id'];
+            delete d['id']
+        })
+       
+        // removing old product data from database
+        await Product.remove({});
+        // creating new product data in database
+        await Product.create(parsedResult.data);
+
+
+        console.log('Product data loaded from mock csv file successfully');
+
+    }catch(error){
+        console.log(error);
+    }
+
+}
+
+createDatabase()
+
+app.listen(PORT, () => {
+    console.log('Server listening on PORT : ' + PORT);
 })
